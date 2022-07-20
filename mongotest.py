50b80710beb6e15b7773752fd8bbd25c0a3d4b91
Murali <1murali5teja@gmail.com>
2022-07-21 00:05:19 +0530

    This is my first code push ingit

---
+++ mongotest.py
@@ -0,0 +1,14 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://murali:mongodb123@cluster0.fxgp1cv.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+print(db)
+
+d ={
+   "name": "murali",
+    "email":"1murali5teja@gmail.com",
+    "surname" :"murali"
+}
+db1 = client['mongotest']
+coll = db1['test']
+coll.insert_one(d)
